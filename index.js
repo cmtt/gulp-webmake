fd5b727a3cc57f4610c16d05818f5157cdc1289a
Joe Esposito <joe@joeyespo.com>
2014-07-05 02:30:10 -0400

    Cleanup
    * Show error on streams, since they're not actually supported
    * Remove unused function names from anonymous functions
    * Use callback functions directly instead of local names
    * Move options || {} directly into the _extend call
    * Use PluginError directly
    * Add missing semicolon
    * Spacing

--- index.js
+++ index.js
@@ -5,25 +5,29 @@ var util = require('util');
 var path = require('path');
 var PluginError = gutil.PluginError;
 
-module.exports = function Webmake (options) {
-  options = options || {};
-
+module.exports = function(options) {
   var defaultOptions = {};
-  options = util._extend(defaultOptions, options);
+  options = util._extend(defaultOptions, options || {});
 
-  return through(_process, gutil.noop);
-
-  function _process (file) {
+  return through(function(file) {
     var self = this;
-    if (file.isNull()) return self.push(null);
 
-    function callback (err, contents) {
-      if (err) return self.emit('error',new gutil.PluginError('gulp-webmake', err, { showStack : true }));
+    if (file.isNull()) {
+      return self.push(null);
+    }
+
+    if (file.isStream()) {
+      self.emit('error', new PluginError('gulp-webmake', 'Streaming not supported.'));
+      return next();
+    }
+
+    webmake(file.path, options, function(err, contents) {
+      if (err) {
+        return self.emit('error', new PluginError('gulp-webmake', err, { showStack: true }));
+      }
       var output = file.clone(file);
       output.contents = new Buffer(contents);
       self.emit('data', output);
-    }
-    if (file.isStream()) webmake(s, options, callback);
-    else webmake(file.path, options, callback);
-  }
-}
+    });
+  }, gutil.noop);
+};
